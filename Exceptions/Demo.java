ef2a857475792319b6ac6c335777f0cebe87f802
EashwarSS <eashwarlucky@gmail.com>
2025-11-11 17:13:16 +0530

    java

---
+++ Exceptions/Demo.java
@@ -0,0 +1,15 @@
+
+// Types of Errors in Java:
+// 1. Complile - time error
+// 2. Runtime error -> Exception handling
+// 3. Logical error
+
+
+public class Demo {
+    public static void main(String[] args) {
+    
+//  	System.out.Println();
+    	System.out.println(2+2);
+    	
+    }
+}
